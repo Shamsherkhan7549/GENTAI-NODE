548bea1518065491273f7a15fedcb6cb2e8a6965
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-11-30 09:13:44 -0800

    day 9 / 30 add data in collection

--- day-9-LLM/server.js
+++ day-9-LLM/server.js
@@ -2,9 +2,19 @@ import dotenv from 'dotenv';
 import { GoogleGenAI } from '@google/genai';
 import { CloudClient } from 'chromadb';
 
+
 dotenv.config();
 
 const googleClient = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
+
+async function createEmbedding(noun) {
+    const response = await googleClient.models.embedContent({
+        model:"gemini-embedding-001",
+        contents:noun
+    })
+    return response.embeddings[0].values
+}
+
 
 const chromaClient = new CloudClient({
     apiKey:process.env.CHROMA_API_KEY,
@@ -12,11 +22,20 @@ const chromaClient = new CloudClient({
     database:process.env.CHROMA_DATABASE
 });
 
-
 async function connectChroma(){
+    const noun = "pegion"
+    const id = "3"
+   const embededData = await createEmbedding(noun);
+
    const chromaConnectioinRes =  await chromaClient.getOrCreateCollection({
-        name:"my_collection3"
-    }); 
+        name:"birds"
+    });
+    
+    chromaConnectioinRes.add({
+        ids:[id],
+        documents:[noun],
+        embeddings:[embededData]
+    })
     
     console.log("collection name = ", chromaConnectioinRes._name);
     
