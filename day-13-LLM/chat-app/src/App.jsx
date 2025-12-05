a88827a56cd574c7c7d3a39c4520dd26a60709df
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-12-05 11:21:03 -0800

    day 13 / 30 showing source of ui responses on ui

--- day-13-LLM/chat-app/src/App.jsx
+++ day-13-LLM/chat-app/src/App.jsx
@@ -7,7 +7,10 @@ const url = import.meta.env.VITE_BACKEND_URL;
 const App = () => {
 
   const [question, setQuestion] = useState("");
-  const [contents, setContents] = useState([]);
+  const [questionShow, setQuestionShow] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [source, setSource] = useState([]);
+  const[showSources, setShowSources] = useState(false);
 
   const handleChange = (e) => {
     setQuestion(e.target.value);
@@ -16,9 +19,13 @@ const App = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${url}/chat`, {question:question});
-      if(response.data.success===true){        
-            apiCall() 
+      setQuestionShow(question);
+
+      const response = await axios.post(`${url}/chat`, { question: question });
+
+      if (response.data.success === true) {
+        setAnswer( (response.data.answer.result))
+        setSource([response.data.answer.sources])
       }
       setQuestion("");
 
@@ -27,39 +34,65 @@ const App = () => {
     }
   }
 
-  const apiCall = async() => {
+  const apiCall = async () => {
 
     const response = await axios.get(`${url}/chat`);
-    
-    if(response.data.success===true){      
-      setContents(response.data.answer);  
-    }    
+
+    if (response.data.success === true) {
+      setContents(response.data.answer);
+    }
 
   }
 
-  useEffect(()=>{
-    apiCall()
-  },[])
+  const handleShowSource = ()=>{
+    if(showSources===true) setShowSources(false)
+      else setShowSources(true)
+  }
+
+  useEffect(() => {
+    // apiCall()
+
+  }, [])
 
 
   return (
     <div className="chat-container">
       <div className="messages">
-        { contents &&
-          contents.map((item, index)=>(
-            <div className='item-box' key={index}>
-               {item.role==="user" &&
-                  <div className="user border rounded p-3">{item.content}</div>
+        {questionShow && answer &&
+
+          <div className='item-box'>
+            { questionShow &&
+              <div className="user border rounded p-3"> {questionShow}</div>}
+            { answer &&
+              <div className="assistant  border rounded p-3 bg-grey-200">
+                {answer}
+
+                <div>
+                  source:
+                {
+                  source[0] &&
+                  source[0].map((ele,idx)=>(
+                    <span key={idx} className='source-id' onClick={handleShowSource}>
+                      {ele.id},
+
+                      { showSources &&
+                        <>
+                        <p className='source-text'>{ele.text}</p>
+                        <p>---------------------------------------------------------------------                        </p>
+                        </>
+                      } 
+                    </span>
+                    
+                  ))
                 }
-                
-               {item.role==="assistant" && 
-                <div className="assistant  border rounded p-3 bg-grey-200">{item.content}</div>
-               }
-               
-            </div>
-          ))
+                </div>
+              </div>}
+
+              
+          </div>
+
         }
-       
+
       </div>
 
       <form onSubmit={handleSubmit} className='input-row d-flex align-items-center px-3'>
